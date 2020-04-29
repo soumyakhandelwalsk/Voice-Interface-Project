f3d7304382aa7d42d0340b649326424e9b2d90e2
Soumya Khandelwal <soumyakhandelwal.sk@gmail.com>
2020-04-29 15:02:30 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -13,30 +13,11 @@
   {
     if(myRec.resultValue) 
     {
-      console.log(myRec)
-      if (flag==1)
-      {
-        if (myRec.resultString=="back")
-        {
-          news(" news ");
-        }
-        else
-        {
-          news_find(myRec.resultString);
-        }
-      }
-      else
-      {
-        text_color();
-        reveal();
-        text_size();
-        scroll_screen();
-        news(" "+myRec.resultString+" ");
-        if (flag==0)
-        {
-          p1.insertAdjacentHTML('beforeend', myRec.resultString + " ");
-        }
-      }
+      text_color();
+      reveal();
+      text_size();
+      scroll_screen();
+      p1.insertAdjacentHTML('beforeend', myRec.resultString + " ");
     }
   }
 
@@ -80,89 +61,4 @@
     });
   }
 
-  function news(word)
-  {
-    if (word.search(" news ")!=-1)
-    {
-      document.getElementById('p1').innerHTML = "";
-      var today = get_time().substring(11,19);
-      document.getElementById('time').innerHTML = today;
-      document.getElementById('first').innerHTML = "";
-      document.getElementById('second').innerHTML = "";
-      document.getElementById('key').innerHTML = "Time<br>Space";
-      document.getElementById("time").style.color = "black";
-      flag = 1;
-    }
-  }
-  function news_find(keyword)
-  {
-    document.getElementById('key').innerHTML = "Back<br>Time<br>Space";
-    var n = get_time();
-    n = n.substring(0,n.length-5);
-    $(document).ready(function(){
-    let url = 'http://newsapi.org/v2/everything?' +
-        'qInTitle='+keyword+'&' +
-        'from='+n+'&' +
-        'sortBy=popularity&' +
-        'pageSize=100&' +
-        'language=en&' +
-        'apiKey=6764826de72f46929193fac403917aea';
-    $.ajax({
-      url:url,
-      method:"GET",
-      dataType:"Json",
-
-      success: function(news){
-        console.log(news);
-        document.getElementById('time').style.color = "red";
-        document.getElementById('time').innerHTML ="";
-        for(var i = 0; i<100; i++)
-        {
-          word = keyword;
-          var str = news.articles[i].title;
-          if (str.search(keyword)==-1)
-          {
-            word = capitalise(" "+keyword);
-          }
-          var s = str.substring(0,str.search(word));
-          var t = str.substring(str.search(word)+word.length)
-          var published = news.articles[i].publishedAt.substring(11,16);
-          var result = s.link(news.articles[i].url);
-          first.insertAdjacentHTML('beforeend', "<br>"+result);
-          time.insertAdjacentHTML('beforeend', "<br>"+"&nbsp;"+word+"&nbsp;");
-          var result = t.link(news.articles[i].url);
-          var h = document.getElementById("second");
-          h.insertAdjacentHTML('beforeend', "<br>"+result + " - "+published);
-        }
-      },
-      error: function(){
-        $("time").html("Some error occured");
-      }
-    })
-    });
-  }
   
-  function get_time()
-  {
-    var today = new Date();
-    today.setMinutes(today.getMinutes()-today.getTimezoneOffset());
-    today.setDate(today.getDate()-1);
-    return today.toISOString();
-  }
-
-  function capitalise(str)
-  {
-    s="";
-    for (var i=1; i<str.length; i++)
-    {
-        if (str.charAt(i-1)==" ")
-        {
-          s = s + str[i].toUpperCase();
-        }
-        else
-        {
-          s=s+str.charAt(i);
-        }
-    }
-    return s;
-  }
